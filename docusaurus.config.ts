d848328b319300b7ed2fef007d69358ccbe595c3
bigmoby <ing.fabiomauro@gmail.com>
2025-11-12 17:12:59 +0100

    fixing workflow

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,10 +15,10 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://docs.ai-gateway.radicalbit.ai/',
+  url: 'https://docs.ai-gateway.radicalbit.ai',
   // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/radicalbit-ai-gateway-docs/',
+  // For custom domain, use '/' (for GitHub pages without custom domain, use '/<projectName>/')
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
